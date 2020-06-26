9507effc65460502f70ff56f6f454ccf95708466
AkberKhan <akber.skypotential@gmail.com>
2020-06-26 13:11:49 +0500

    First Package

---
+++ src/simple.php
@@ -0,0 +1,10 @@
+<?php
+namespace Akberkhanskypotential\Simple;
+
+class Simple
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
